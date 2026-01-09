47412991639b0b3922b999628708715e2ef696b6
Adam-Warlock09 <aricm1809@gmail.com>
2026-01-09 13:59:16 +0530

    Merch additions added

--- src/app/merchandise/page.tsx
+++ src/app/merchandise/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { useState, useEffect, useRef } from "react";
 import { Play, Pause } from "lucide-react";
 
-const shirts = [
+const items = [
   {
     id: 1,
     subname: "PREMIUM SWEATSHIRT",
@@ -61,10 +61,121 @@ const shirts = [
     buy_link:
       "https://thedop-aminestore.myshopify.com/products/iit-hyderabad-tote-bag-cool-it?sca_ref=10010623.NVQXYZ0k2hwYBZG",
   },
+  {
+    id: 6,
+    subname: "PREMIUM HOODIE",
+    name: "THE RONIN",
+    price: "RS. 799.00",
+    image: "/images/shirt6.png",
+    image_org: "/images/shirt6_org.png",
+    jp_text: "/images/shirt6_jp.png",
+    buy_link: "https://thedopaminestore.in/products/the-ronin-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 7,
+    subname: "PREMIUM HOODIE",
+    name: "DEMONIC GEISHA",
+    price: "RS. 799.00",
+    image: "/images/shirt7.png",
+    image_org: "/images/shirt7_org.png",
+    jp_text: "/images/shirt7_jp.png",
+    buy_link: "https://thedopaminestore.in/products/demonic-geisha-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 8,
+    subname: "PREMIUM HOODIE",
+    name: "BATTLEFLARE",
+    price: "RS. 799.00",
+    image: "/images/shirt8.png",
+    image_org: "/images/shirt8_org.png",
+    jp_text: "/images/shirt8_jp.png",
+    buy_link: "https://thedopaminestore.in/products/battleflare-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 9,
+    subname: "TOTE BAG",
+    name: "THE TWIN DEMONS",
+    price: "RS. 199.00",
+    image: "/images/shirt9.png",
+    image_org: "/images/shirt9_org.png",
+    jp_text: "/images/shirt9_jp.png",
+    buy_link: "https://thedop-aminestore.myshopify.com/products/the-twin-demons-tote-bag-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 10,
+    subname: "TOTE BAG",
+    name: "RONIN OF THE RED (BLACK)",
+    price: "RS. 199.00",
+    image: "/images/shirt10.png",
+    image_org: "/images/shirt10_org.png",
+    jp_text: "/images/shirt10_jp.png",
+    buy_link:
+      "https://thedop-aminestore.myshopify.com/products/ronin-of-the-red-tote-bag-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 11,
+    subname: "OVERSIZED TEE",
+    name: "SOLAR CRANE (IVORY)",
+    price: "RS. 499.00",
+    image: "/images/shirt11.png",
+    image_org: "/images/shirt11_org.png",
+    jp_text: "/images/shirt11_jp.png",
+    buy_link: "https://thedop-aminestore.myshopify.com/products/solar-crane-oversized-t-shirt-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 12,
+    subname: "PREMIUM SHIRT",
+    name: "THE RONIN",
+    price: "RS. 349.00",
+    image: "/images/shirt12.png",
+    image_org: "/images/shirt12_org.png",
+    jp_text: "/images/shirt12_jp.png",
+    buy_link: "https://thedop-aminestore.myshopify.com/products/the-ronin-t-shirt-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 13,
+    subname: "PREMIUM SHIRT",
+    name: "DEMONIC GEISHA",
+    price: "RS. 349.00",
+    image: "/images/shirt13.png",
+    image_org: "/images/shirt13_org.png",
+    jp_text: "/images/shirt13_jp.png",
+    buy_link: "https://thedopaminestore.in/products/demonic-geisa-t-shirt-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 14,
+    subname: "PREMIUM SHIRT",
+    name: "BATTLEFLARE",
+    price: "RS. 349.00",
+    image: "/images/shirt14.png",
+    image_org: "/images/shirt14_org.png",
+    jp_text: "/images/shirt14_jp.png",
+    buy_link: "https://thedop-aminestore.myshopify.com/products/battleflare-tshirt-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 15,
+    subname: "WHITE TOTE BAG",
+    name: "LADY OF THE BREATH",
+    price: "RS. 199.00",
+    image: "/images/shirt15.png",
+    image_org: "/images/shirt15_org.png",
+    jp_text: "/images/shirt15_jp.png",
+    buy_link: "https://thedop-aminestore.myshopify.com/products/lady-of-the-breath-tote-bag-iit-hyd?sca_ref=10010623.NVQXYZ0k2hwYBZG",
+  },
+  {
+    id: 16,
+    subname: "MOUSEPAD",
+    name: "ENV",
+    price: "RS. 199.00",
+    image: "/images/shirt16.png",
+    image_org: "/images/shirt16_org.png",
+    jp_text: "/images/shirt16_jp.png",
+    buy_link: "https://thedopaminestore.in/collections/iit-hyderabad?sca_ref=10010623.NVQXYZ0k2hwYBZG&sca_source=Main%20website",
+  },
 ];
 
 export default function Merchandise() {
-  const [selectedShirt, setSelectedShirt] = useState(shirts[0]);
+  const [selectedShirt, setSelectedShirt] = useState(items[0]);
   const [mounted, setMounted] = useState(false);
   const [visibleElements, setVisibleElements] = useState<Set<string>>(
     new Set()
@@ -325,9 +436,11 @@ export default function Merchandise() {
             </div>
           </a>
 
-          {/* Shirts Grid on Right Side */}
-          <div className="absolute top-[14vh] right-[3vw] grid grid-cols-2 w-auto gap-x-[1vw] gap-y-[1vw]">
-            {shirts.map((shirt) => (
+          {/* items Grid on Right Side */}
+          <div
+            className={`overflow-y-auto w-auto absolute top-[14vh] h-[80vh] right-[3vw] grid grid-cols-2 gap-x-[1vw] gap-y-[1vw]`}
+          >
+            {items.map((shirt) => (
               <button
                 key={shirt.id}
                 onClick={() => setSelectedShirt(shirt)}
@@ -342,7 +455,7 @@ export default function Merchandise() {
                   alt={shirt.name}
                   fill
                   sizes="15vw"
-                  className="object-contain"
+                  className="object-fill"
                 />
                 <a href={shirt.buy_link}>
                   <div className="absolute bottom-[15vh] right-[1vh] text-white text-[1.5vw] uppercase hover:scale-110 transition-all duration-300">
@@ -370,7 +483,7 @@ export default function Merchandise() {
             ref={mobileCarouselRef}
             className="flex-1 flex overflow-x-auto overflow-y-hidden snap-x snap-mandatory scroll-smooth scrollbar-hide relative p-0"
           >
-            {shirts.map((shirt) => (
+            {items.map((shirt) => (
               <div
                 key={shirt.id}
                 className="shrink-0 w-[85vw] h-[72vh] flex flex-col items-center justify-center snap-center"
