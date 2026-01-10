d0646107a747b95bebdf91149a492598b07321cd
nathanalvares19 <nathanalvares2706@gmail.com>
2026-01-10 11:43:58 +0530

    revert downtime message

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,7 +4,12 @@ import { useEffect } from "react";
 
 export default function Alert() {
   useEffect(() => {
-    alert("Fest Pass Update:\n\n" + "Downtime: 5 minutes");
+    alert(
+      "Fest Pass Update:\n\n" +
+        "The fest pass mailing system is experiencing heavy traffic. " +
+        "Emails may take around 15 minutes to reach you. " +
+        "Also ensure that your inbox has enough storage to receive the mail."
+    );
   }, []);
 
   return null;
