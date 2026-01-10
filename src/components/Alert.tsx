ef7f85c0641a1a749166ea0acdbb190178d740c4
nathanalvares19 <nathanalvares2706@gmail.com>
2026-01-10 11:39:20 +0530

    downtime

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,12 +4,7 @@ import { useEffect } from "react";
 
 export default function Alert() {
   useEffect(() => {
-    alert(
-      "Fest Pass Update:\n\n" +
-        "The fest pass mailing system is experiencing heavy traffic. " +
-        "Emails may take around 15 minutes to reach you. " +
-        "Also ensure that your inbox has enough storage to receive the mail."
-    );
+    alert("Fest Pass Update:\n\n" + "Downtime: 5 minutes");
   }, []);
 
   return null;
